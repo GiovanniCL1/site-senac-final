7ff9d151e957a5f87cf735864a2c51bbd71ced07
GiovanniCL1 <119444264+GiovanniCL1@users.noreply.github.com>
2023-02-09 14:08:44 -0300

    Add files via upload

---
+++ cursos.js
@@ -0,0 +1,57 @@
+var alunos = ["Pedro", "Luiz Felipe", "Giovanni", "Arthur", "Vanderlei", "Jao Vitor", "Cecilia", "Maria", "Joao vitor"]
+var idade = [26, 16, 17, 17, 16, 16, 17, 16, 17]
+
+var programacao ={
+    nome: "Programaçao",
+    descricao: "Cursos para quem tem interesse em ingressar na área de preogramacão.",
+    topicos: ["Tecnologia", "Design UI/UX", "Desenvolvimento Web"],
+    imagem: "https://www.ideianoar.com.br/wp-content/uploads/2021/09/desenvolvedor-web.jpg"
+}
+
+var administracao ={
+    nome: "Administração",
+    descricao: "Direcionado para quem queria trabalhar no setor administrativo e com finanças.",
+    topicos: ["Administração", "Marketing", "Finanças e Contabilidade"],
+    imagem: "file:///C:/Users/962395/Downloads/administracao.jpg"
+}
+
+var enfermagem ={
+    nome: "Enfermagem",
+    descricao: "Para voce que gostaria de ingressar na área da sáude.",
+    topicos: ["Biosegurança", "Primeiros socorros", "Ministração de medicamentos"],
+    imagem: "file:///C:/Users/962395/Downloads/enfermagem.jpg"
+}
+
+var cursos = [programacao, administracao, enfermagem]
+console.log (cursos)
+
+/*
+var titulo = document.getElementById("titulo");
+titulo.style.backgroundColor = "green"
+titulo.style.color = "black"
+saibamais.style.backgroundColor = "green"
+span.style.backgroundColor = "red"
+
+var programacao =document.getElementById("programacao")
+programacao.innerHTML = "<h1>Programação</h1>"
+
+
+var topicos = document.getElementById("topicos")
+console.log(topicos)
+
+var liTopicos = document.createElement("li")
+liTopicos.innerText = "Front-end"
+console.log(liTopicos)
+
+topicos.appendChild(liTopicos)
+
+
+var haderBotoes = document.getElementById("lista-botoes");
+
+var botaoHader = document.createElement("li")
+botaoHader.innerHTML = `<a href="index.html">
+<li>Início</li> </a>`
+
+haderBotoes.appendChild(botaoHader)
+*/
+
